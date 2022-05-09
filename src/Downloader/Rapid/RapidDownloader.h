c768a82f486e7f8897fa6e6cd656f4df9c9ed5ad
eru <mysterme@gmail.com>
2022-05-09 10:26:43 +0200

    Fix REPO_MASTER

--- src/Downloader/Rapid/RapidDownloader.h
+++ src/Downloader/Rapid/RapidDownloader.h
@@ -12,7 +12,7 @@
 #define REPO_MASTER_RECHECK_TIME \
 	86400 // how long to cache the repo-master file in secs without rechecking
 #define REPO_RECHECK_TIME 0
-#define REPO_MASTER "http://localhost/repos.gz"
+#define REPO_MASTER "http://repos.springrts.com/repos.gz"
 
 class CSdp;
 class CHttpDownload;
